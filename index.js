8654572691a22c01ad56df110c3e5101f8a33607
Connor Kormos <connorkormos97@gmail.com>
2023-02-23 16:33:19 -0800

    complete

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded!")
+    changeText();
+});
+
+const changeText = () => document.querySelector('#text').textContent = "This is really cool!";
